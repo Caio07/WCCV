ec488a167d284c8d7aa5cc01b4d4ac69bad8393d
caio07 <caio.oliveira07@gmail.com>
2016-02-18 16:37:06 -0200

    Mapa inserido e What We do finalizado
    Mapa do Google Maps inserido
    Headers colocados para alinhamento da borda com o título
    Área What We Do finalizada

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
+/* Mapa */
 
+function init_map(){
+	var myOptions = {
+		zoom:10,
+		center:new google.maps.LatLng(1.1577917,30.734995000000026),
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		scrollwheel: false
+		};
+		map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);
+		marker = new google.maps.Marker({
+			map: map,
+			position: new google.maps.LatLng(1.1577917,30.734995000000026)
+		});
+		
+		
+
+		google.maps.event.addListener(marker, 'click',
+
+		function(){
+			infowindow.open(map,marker);
+		});
+			infowindow.open(map,marker);
+		}
+
+
+
+		google.maps.event.addDomListener(window, 'load', init_map);
